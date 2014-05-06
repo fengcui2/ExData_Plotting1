25b8d3f07a4a1fd7adfbbe0fd1d274628a468917
fengcui2 <fengcui2@yahoo.com>
2014-05-06 15:56:04 -0400

    Delete plot1.R

--- plot1.R
+++
@@ -1,9 +0,0 @@
-path = getwd()
-directory = "exdata-data-household_power_consumption"
-id = "household_power_consumption"
-file <- paste(path, '/', directory, '/', id, '.txt', sep="")
-
-data <-read.table(file, header = TRUE, sep=';', na.strings="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-data_new <- subset(data, Date %in% c("1/2/2007", "2/2/2007"))
-
-hist(data_new$Global_active_power, col = 'red', main = "Global Active Power", xlab = "Global Active Power (kilowatts)", cex.axis = 0.7, cex.lab = 0.7, cex.main = 0.7)
