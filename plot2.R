702dc308494ebdbf2fb7b13b91c2bcd361eb80c3
fengcui2 <fxcsbi@COS-BIO22.main.ad.rit.edu>
2014-05-06 18:22:17 -0400

    Plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+
+path = getwd()
+directory = "exdata-data-household_power_consumption"
+id = "household_power_consumption"
+file <- paste(path, '/', directory, '/', id, '.txt', sep="")
+
+data <-read.table(file, header = TRUE, sep=';', na.strings="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+data_new <- subset(data, Date %in% c("1/2/2007", "2/2/2007"))
+
+my_time <- paste(data_new$Date, data_new$Time)
+
+my_time_new <- strptime(x, "%d/%m/%Y %H:%M:%S")
+
+filename <- paste(path , '/' , "plot2.png", sep="")
+png(filename, width = 480, height = 480)
+plot(my_time_new, data_new$Global_active_power, type="n", ylab = "Global Active Power (kilowatts)", xlab = "")
+lines(my_time_new, data_new$Global_active_power, type = "l")
+dev.off()
